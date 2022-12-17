01005b212a1b6dd07f97bb0862b1f1f911c62700
Matthew Lea <matthewlea8@gmail.com>
2022-12-17 18:13:19 +1100

    added basic route, and 1 test

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const router = require("./controllers/router")
+
+const app = express();
+
+const PORT = 3000
+
+app.use(express.static("static"));
+app.use(bodyParser.json());
+app.use("/", router);
+
+
+// app.get("/api/hello", (req, res) => {
+//     res.json({status: "ok"})
+// })
+
+app.listen(PORT, () => {
+    console.log(`Can connect on http://localhost:${PORT}`)
+})
