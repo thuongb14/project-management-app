dfdab715818c9212dae65277a59addf5a9db7c5b
thuongb14 <thuongb14@gmail.com>
2022-12-22 19:17:13 +1100

    fix small bug

--- static/js/initialize.js
+++ static/js/initialize.js
@@ -1,20 +1,20 @@
-import { renderHeader } from "./components/header.js";
-import { getstartedButton } from "./components/getStarted.js"
-import { whyUsLink, featuresLink, blogLink } from "./components/navLinks.js"
-import {renderLandingPage} from "./components/landingPage.js"
+import { renderHeader } from './components/header.js';
+import { getstartedButton } from './components/getStarted.js';
+import { blogLink } from './components/navLinks.js';
+import { renderLandingPage } from './components/landingPage.js';
 //initial rendering
-renderHeader()
-renderLandingPage()
+renderHeader();
+renderLandingPage();
 
 //header on click
-blogLink()
+blogLink();
 
 //other element on click
 window.addEventListener('click', (e) => {
-    if (e.target.id === 'pLogo') {
-        renderLandingPage()
-    }
-    if (e.target.id === 'sign-up') {
-        getstartedButton()
-    }
-})
+  if (e.target.id === 'pLogo') {
+    renderLandingPage();
+  }
+  if (e.target.id === 'sign-up') {
+    getstartedButton();
+  }
+});
