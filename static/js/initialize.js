d7b51d29ef1542ee1e0114480e78668d01ac95bf
vishula1 <vgamaetige@gmail.com>
2022-12-22 17:26:43 +1000

    Login page merge resolve

--- static/js/initialize.js
+++ static/js/initialize.js
@@ -1,19 +1,6 @@
 import { renderHeader } from "./components/header.js";
 import { getstartedButton } from "./components/getStarted.js"
-<<<<<<< HEAD
-import {renderLogin} from "./components/login.js"
+ 
 renderHeader()
 getstartedButton()
-renderLogin()
-=======
-import { whyUsLink, pmaLogo, featuresLink, blogLink } from "./components/navLinks.js"
-
-
-
-renderHeader()
-getstartedButton()
-whyUsLink()
-pmaLogo()
-featuresLink()
-blogLink()
->>>>>>> 94f5ea3bc3317afca1aace393a354908e4579487
+ 
