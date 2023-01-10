e705a7396202ba3d6a4cfd74732b24cc5a818dc4
Matthew Lea <matthewlea8@gmail.com>
2023-01-10 20:11:38 +1100

    added guest login button to login page

--- database/seed.sql
+++ database/seed.sql
@@ -1,1 +1,1 @@
-INSERT INTO users (name, email, password) VALUES ('Guest', 'guest@example.com', 'guest123')
+INSERT INTO users (name, email, password) VALUES ('Guest', 'guest@example.com', '$2b$10$pg35WVjL0qBpmsVpIRJb.u7LdFyfUjPEHhzKLPRgEB887RlkD0paG')
